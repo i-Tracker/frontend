2600f87cc9a9ea36822fb1f78f2eda0f71a314d8
gyeongza <gyeongza@naver.com>
2024-05-17 15:04:59 +0900

    refactor: category page 컴포넌트 이름 수정

--- src/app/(iTracker)/category/page.tsx
+++ src/app/(iTracker)/category/page.tsx
@@ -1,6 +1,6 @@
 import { Text } from '@/shared/components/shadcn/Text';
 
-export default async function Home() {
+export default async function Category() {
   return (
     <div className="flex-1 w-full flex flex-col gap-10 items-center">
       <Text typography="h2">카테고리 페이지 입니다.</Text>
