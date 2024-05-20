3939678bc60efc791de3ed367ed3a896463bec0c
gyeongza <gyeongza@naver.com>
2024-05-20 23:09:01 +0900

    stype: 불필요한 import 삭제

--- src/app/(iTracker)/product/[categoryName]/page.tsx
+++ src/app/(iTracker)/product/[categoryName]/page.tsx
@@ -1,7 +1,6 @@
 import { categoryMap, categoryType } from '@/features/category/constants';
 import { ProductList } from '@/features/product/components';
 import { Text } from '@/shared/components/shadcn/Text';
-import { convertToLowerCaseWithoutSpace } from '@/shared/utils';
 
 export default function CategoryNamePage({ params }: { params: { categoryName: categoryType } }) {
   const categoryName = categoryMap[params.categoryName];
