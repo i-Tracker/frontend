824f5f946c2c0a37a04151d35fe346cb9faf10d2
gyeongza <gyeongza@naver.com>
2024-05-31 17:09:52 +0900

    design: dialog footer 모바일 마진값 추가

--- src/shared/components/shadcn/ui/dialog.tsx
+++ src/shared/components/shadcn/ui/dialog.tsx
@@ -62,7 +62,10 @@ const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivEleme
 DialogHeader.displayName = 'DialogHeader';
 
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
+  <div
+    className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 sm:gap-0 gap-3', className)}
+    {...props}
+  />
 );
 DialogFooter.displayName = 'DialogFooter';
 
