2a584ed3073b83257a83fd72f6a875c75f9d2f91
gyeongza <gyeongza@naver.com>
2024-05-31 16:52:06 +0900

    design: toggle 선택 색상 및 isChecked 속성 추가

--- src/shared/components/shadcn/ui/toggle-group.tsx
+++ src/shared/components/shadcn/ui/toggle-group.tsx
@@ -25,22 +25,26 @@ ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName;
 
 const ToggleGroupItem = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> & VariantProps<typeof toggleVariants>
->(({ className, children, variant, size, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &
+    VariantProps<typeof toggleVariants> & {
+      isChecked: boolean;
+    }
+>(({ className, children, variant, size, isChecked, ...props }, ref) => {
   const context = React.useContext(ToggleGroupContext);
 
+  const itemClassName = cn(
+    toggleVariants({
+      variant: context.variant || variant,
+      size: context.size || size,
+    }),
+    {
+      'bg-primary text-white': isChecked, // 예시: 체크된 상태일 때 파란 배경과 흰 글씨 적용
+    },
+    className,
+  );
+
   return (
-    <ToggleGroupPrimitive.Item
-      ref={ref}
-      className={cn(
-        toggleVariants({
-          variant: context.variant || variant,
-          size: context.size || size,
-        }),
-        className,
-      )}
-      {...props}
-    >
+    <ToggleGroupPrimitive.Item ref={ref} className={itemClassName} {...props}>
       {children}
     </ToggleGroupPrimitive.Item>
   );
