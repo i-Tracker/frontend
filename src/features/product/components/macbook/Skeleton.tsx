04597e4ebe1b5ef97c740d9b0c74be42ca17868f
gyeongza <gyeongza@naver.com>
2024-06-04 17:21:57 +0900

    design: skeleton 디자인 수정

--- src/features/product/components/macbook/Skeleton.tsx
+++ src/features/product/components/macbook/Skeleton.tsx
@@ -4,7 +4,7 @@ export const MacbookProductListSkeleton = () => {
   const items = Array.from({ length: 5 }, (_, i) => <ProductItemSkeleton key={i} />);
 
   return (
-    <div className="mx-auto">
+    <div>
       <ul className="whitespace-nowrap overflow-x-auto flex gap-6 my-6">{items}</ul>
     </div>
   );
