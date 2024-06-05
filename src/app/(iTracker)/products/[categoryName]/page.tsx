2c789f34b7087b383df1765eb1b25fe76cd74e60
gyeongza <gyeongza@naver.com>
2024-06-05 12:35:09 +0900

    refactor: filter variant 추가

--- src/app/(iTracker)/products/[categoryName]/page.tsx
+++ src/app/(iTracker)/products/[categoryName]/page.tsx
@@ -2,5 +2,5 @@ import { CategoryType } from '@/features/category/constants';
 import { Filter } from '@/features/search/components/filter';
 
 export default function CategoryNamePage({ params }: { params: { categoryName: CategoryType } }) {
-  return <Filter title="상품 필터" category={params.categoryName} />;
+  return <Filter title="상품 필터" category={params.categoryName} variant={false} />;
 }
