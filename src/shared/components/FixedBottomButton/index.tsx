0902005f692cf8924f931571491e3a5521479ddb
gyeongza <gyeongza@naver.com>
2024-06-07 04:17:03 +0900

    feat: FixedBottomButton 추가

---
+++ src/shared/components/FixedBottomButton/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { Button } from '../shadcn/ui/button';
+
+interface FixedBottomButtonProps {
+  title: string;
+  link: string;
+}
+
+export const FixedBottomButton = ({ title, link }: FixedBottomButtonProps) => {
+  return (
+    <div className="w-full flex items-center">
+      <Link href={link}>
+        <Button size="lg" className="w-full fixed bottom-0 left-0 right-0 rounded-none py-6">
+          {title}
+        </Button>
+      </Link>
+    </div>
+  );
+};
