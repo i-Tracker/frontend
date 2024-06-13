ef710a7d352c16eff36eaa53aae1679916b2cc9b
gyeongza <gyeongza@naver.com>
2024-06-13 14:05:12 +0900

    feat: 준비중인 카테고리 disabled

--- src/features/category/components/CategoryList/Item/index.tsx
+++ src/features/category/components/CategoryList/Item/index.tsx
@@ -1,7 +1,7 @@
 import { categoryMap, CategoryType } from '@/features/category/constants';
 import { Text } from '@/shared/components/shadcn/Text';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 interface CategoryItemProps {
   categoryItem: CategoryType;
@@ -9,10 +9,27 @@ interface CategoryItemProps {
 
 export const CategoryItem = ({ categoryItem }: CategoryItemProps) => {
   const categoryName = categoryMap[categoryItem];
+  const router = useRouter();
+
+  const isDisabledCategory = ['mac', 'apple_watch', 'i_phone', 'i_pad'].includes(categoryItem);
+  const handleClickCategory = () => {
+    if (isDisabledCategory) {
+      alert('준비중인 카테고리입니다.');
+      return;
+    }
+
+    router.push(`/category/${categoryItem}`);
+  };
+
+  const categoryClass = isDisabledCategory ? 'opacity-30' : '';
 
   return (
-    <li>
-      <Link href={`/category/${categoryItem}`} className="flex flex-col items-center gap-2 cursor-pointer">
+    <li className={categoryClass}>
+      <div
+        className="flex flex-col items-center gap-2 cursor-pointer"
+        onClick={handleClickCategory}
+        aria-label={categoryItem}
+      >
         <div className="flex items-center justify-center w-[80px] h-[80px] bg-neutral-100 rounded">
           <Image
             src={`/category/${categoryItem}.png`}
@@ -23,7 +40,7 @@ export const CategoryItem = ({ categoryItem }: CategoryItemProps) => {
           />
         </div>
         <Text typography="xsmall">{categoryName}</Text>
-      </Link>
+      </div>
     </li>
   );
 };
