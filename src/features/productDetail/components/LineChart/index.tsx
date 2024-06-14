bb3af6f48586fc8b5f89bda9b21aa1059243d54b
gyeongza <gyeongza@naver.com>
2024-06-14 15:47:30 +0900

    feat: 가격 그래프 최저, 최고, 평균가 추가

--- src/features/productDetail/components/LineChart/index.tsx
+++ src/features/productDetail/components/LineChart/index.tsx
@@ -13,6 +13,7 @@ import {
 } from 'recharts';
 import { CustomTooltip } from './CustomTooltip';
 import { CustomDot } from './CustomDot';
+import { convertToLocalFormat } from '@/shared/utils';
 
 interface PriceChartProps {
   priceInfos: { date: string; currentPrice: number }[];
@@ -22,18 +23,30 @@ interface PriceChartProps {
 }
 
 const PriceChart = ({ priceInfos, allTimeHighPrice, allTimeLowPrice, averagePrice }: PriceChartProps) => {
+  const formatYAxis = (yTick: number) => {
+    return `${convertToLocalFormat(yTick)}`;
+  };
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart data={priceInfos} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis domain={['dataMin', 'dataMax']} />
+        <XAxis type="category" dataKey="date" />
+        <YAxis domain={['dataMin', 'dataMax']} padding={{ top: 24, bottom: 24 }} tickFormatter={formatYAxis} />
         <Tooltip content={<CustomTooltip />} />
         <Legend />
-        <Line type="monotone" dataKey="currentPrice" stroke="#8884d8" activeDot={{ r: 8 }} dot={<CustomDot />} />
-        {/* <ReferenceLine y={allTimeHighPrice} label="최고가" stroke="red" strokeDasharray="3 3" />
+        <Line
+          type="stepBefore"
+          dataKey="currentPrice"
+          stroke="#333333"
+          strokeWidth={2}
+          name="현재가"
+          activeDot={{ r: 8 }}
+          dot={<CustomDot />}
+        />
+        <ReferenceLine y={allTimeHighPrice} label="최고가" stroke="red" strokeDasharray="3 3" />
         <ReferenceLine y={allTimeLowPrice} label="최저가" stroke="green" strokeDasharray="3 3" />
-        <ReferenceLine y={averagePrice} label="평균가" stroke="blue" strokeDasharray="3 3" /> */}
+        <ReferenceLine y={averagePrice} label="평균가" stroke="blue" strokeDasharray="3 3" />
       </LineChart>
     </ResponsiveContainer>
   );
