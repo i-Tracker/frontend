7b7b90868f1058d1977a7222381c44c8351e1906
gyeongza <gyeongza@naver.com>
2024-06-14 16:24:04 +0900

    design: 구매버튼 padding 수정

--- src/shared/components/FixedBottomButton/index.tsx
+++ src/shared/components/FixedBottomButton/index.tsx
@@ -8,18 +8,18 @@ interface FixedBottomButtonProps {
 
 export const FixedBottomButton = ({ title, link }: FixedBottomButtonProps) => {
   return (
-    <div className="w-full flex items-center">
+    <>
       {link ? (
         <Link href={link} target="_blank">
-          <Button size="lg" className="w-full fixed bottom-0 left-0 right-0 rounded-none py-6">
+          <Button size="lg" className="w-full fixed bottom-0 left-0 right-0 rounded-none py-8">
             {title}
           </Button>
         </Link>
       ) : (
-        <Button size="lg" disabled={true} className="w-full fixed bottom-0 left-0 right-0 rounded-none py-6">
+        <Button size="lg" disabled={true} className="w-full fixed bottom-0 left-0 right-0 rounded-none py-8">
           상품 URL이 존재하지 않습니다.
         </Button>
       )}
-    </div>
+    </>
   );
 };
