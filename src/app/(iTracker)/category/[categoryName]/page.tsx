30e9d16d9c3299083cf8d81d3426dc4cabeb7292
gyeongza <gyeongza@naver.com>
2024-06-25 15:52:56 +0900

    refactor: 링크 함수로 변경

--- src/app/(iTracker)/category/[categoryName]/page.tsx
+++ src/app/(iTracker)/category/[categoryName]/page.tsx
@@ -4,13 +4,14 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation'; // Next.js의 라우터를 사용
 import { CategoryType } from '@/features/category/constants';
 import { Filter } from '@/features/search/components/filter';
+import { getCategoryPageLink } from '@/shared/lib/link/page';
 
 export default function CategoryNamePage({ params }: { params: { categoryName: CategoryType } }) {
   const router = useRouter();
 
   useEffect(() => {
     if (params.categoryName === 'airpods') {
-      router.push(`/category/airpods/search`);
+      router.push(`${getCategoryPageLink()}/airpods/search`);
     }
   }, [router, params.categoryName]);
 
