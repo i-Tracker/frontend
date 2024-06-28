83db3113a11a836a11c7a92fdfaecf27caae6e13
gyeongza <gyeongza@naver.com>
2024-06-28 17:53:32 +0900

    refactor: UserInfo 컴포넌트 분리

--- src/shared/components/UserProfile/index.tsx
+++ src/shared/components/UserProfile/index.tsx
@@ -1,15 +1,33 @@
 'use client';
 
+import { Text } from '../shadcn/Text';
 import { Avatar, AvatarFallback, AvatarImage } from '../shadcn/ui/avatar';
 import { useUserInfo } from '@/features/auth/hooks/useUserInfo';
+import { Button } from '../shadcn/ui/button';
+import { logout } from '@/features/auth/api/oauth';
 
 export const UserProfile = () => {
   const { data: userInfo } = useUserInfo();
 
+  const handleClickLogoutButton = () => {
+    logout();
+  };
+
   return (
-    <Avatar>
-      <AvatarImage src={userInfo?.image} alt="profile-image" />
-      <AvatarFallback></AvatarFallback>
-    </Avatar>
+    <div className="flex items-center gap-2">
+      <Avatar>
+        <AvatarImage src={userInfo?.image} alt="profile-image" />
+        <AvatarFallback></AvatarFallback>
+      </Avatar>
+      <Text typography="h4" className="inline">
+        {userInfo?.nickname}
+      </Text>
+      <Text className="inline">님 안녕하세요!</Text>
+      <div>
+        <Button variant="outline" size="sm" onClick={handleClickLogoutButton}>
+          로그아웃
+        </Button>
+      </div>
+    </div>
   );
 };
