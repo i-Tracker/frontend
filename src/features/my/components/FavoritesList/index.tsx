4a38f633312d27b7b14f14ff3a3843b065c94222
gyeongza <gyeongza@naver.com>
2024-07-01 17:42:33 +0900

    design: 알림 상품 없는 경우 디자인 변경

--- src/features/my/components/FavoritesList/index.tsx
+++ src/features/my/components/FavoritesList/index.tsx
@@ -4,19 +4,40 @@ import { Airpods, Macbook } from '@/features/product/api/getProductList';
 import { useGetFavorites } from '../../hooks/useGetFavorites';
 import { MacbookSearchItem } from '@/features/search/components/searchResult/Item/MacbookSearchItem';
 import { AirpodsSearchItem } from '@/features/search/components/searchResult/Item/AirpodsSearchItem';
+import { Text } from '@/shared/components/shadcn/Text';
+import { Button } from '@/shared/components/shadcn/ui/button';
+import { useRouter } from 'next/navigation';
+import { getMainPageLink } from '@/shared/lib/link/page';
 
 const FavoritesList = () => {
   const { data: favorites } = useGetFavorites();
+  const router = useRouter();
 
   if (favorites?.data.length === 0) {
-    return <div>등록된 알림상품이 없습니다.</div>;
+    return (
+      <div className="flex flex-col">
+        <Text typography="xsmall" className="my-4 inline-block">
+          등록된 알림상품이 없습니다.
+        </Text>
+        <div>
+          <Button
+            size="sm"
+            onClick={() => {
+              router.push(getMainPageLink());
+            }}
+          >
+            상품등록 하러가기
+          </Button>
+        </div>
+      </div>
+    );
   }
 
   const renderFavoritesItem = (productItem: Macbook | Airpods) => {
     switch (productItem.category) {
       case 'macbook_air':
       case 'macbook_pro':
-        return <MacbookSearchItem key={productItem.id} productItem={productItem as Macbook} />;
+        return <MacbookSearchItem key={productItem.id} productItem={productItem as Macbook} isFavoriteItem={true} />;
       case 'airpods':
       case 'airpods_max':
       case 'airpods_pro':
